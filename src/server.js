057c0b9dc8c85b14c13d9b1264f35af1b6996c05
vinicius dos santos mota da silva <viniciussantos306@gmail.com>
2019-04-15 00:59:51 -0300

    adiciona variaveis ambiente

--- src/server.js
+++ src/server.js
@@ -34,4 +34,4 @@ app.use(express.urlencoded({
 app.use('/files', express.static(path.resolve(__dirname, '..', 'temp')));
 
 app.use(require('./routes'));
-server.listen(3333);
+server.listen(process.env.PORT || 3333);
